d2541f3c199110e2a214bebe6ee075a3fb668d72
K4m3ledd1in3 <s309439@studenti.polito.it>
2025-03-12 16:24:28 +0100

    ..

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 #include <fstream>
-//exercise 2
+#include <iomanip>
+#include <limits>
+#include <numbers> 
+const int a= 1;
+const int b = 5;
+const int c=-1;
+const int d = 2;
+double AB_to_CD(double v){
+  return 0.75*v-1-0.75 ;
+  //return v*(d-c)/(b-a)-c-a*(d-c)/(b-a);
+}
+
 int main()
 {
-	std::ifstream file("data.txt");
-	if(file.fail()){
-		std::cerr << " error"<<std::endl;
-		return -1;
+
+    std::string filename = "data.txt";
+    std::string filename_out = "result.txt";
+    std::ifstream ifs(filename);
+	std::cout<<std::setprecision(16)<<std::scientific;
+	std::ofstream myfile(filename_out);	 
+	myfile << std::scientific << std::setprecision(16);
+	if (ifs.is_open() || myfile.is_open())
+	{
+	while(!ifs.eof()) 
+	{		
+		double temp;
+		ifs >> temp; 
+		std::cout <<  temp << " - ";
+		myfile << AB_to_CD(temp)<< "\n";
+		std::cout << AB_to_CD(temp) <<"\n";
 	}
-	std::string tmp;
-	std::getline(file,tmp);
-	std::cout << tmp << std::endl;
-	int val;
-    while(file >> val){
-//
+	myfile.close();
 	}
-	
 	return 0;
 }
